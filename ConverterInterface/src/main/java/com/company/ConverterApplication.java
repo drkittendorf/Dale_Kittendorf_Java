9702d15f531906b11576f2d04cd44649f13375f4
Dale Kittendorf <drkittendorf@gmail.com>
2021-06-04 23:49:07 -0500

    scanner interaction cleaned up

--- ConverterInterface/src/main/java/com/company/ConverterApplication.java
+++ ConverterInterface/src/main/java/com/company/ConverterApplication.java
@@ -12,25 +12,25 @@ public class ConverterApplication {
 
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Please enter the MONTH Number for the If/Else to convert:");
+        System.out.println("Please enter the Number you would like the If/Else Converter to convert to a Month: " );
         // return month from switch
         int ifMonth = Integer.parseInt(scanner.nextLine());
-        System.out.println(ConverterIf.convertMonth(ifMonth));
+        System.out.println("Your requested month is: " + ConverterIf.convertMonth(ifMonth) + "\n");
         // enter day number
-        System.out.println("Please enter the DAY Number for the If/Else to convert:");
+        System.out.println("Please enter the Number for the If/Else Converter to convert to a Day:");
         int ifDay = Integer.parseInt(scanner.nextLine());
-        System.out.println(ConverterIf.convertDay(ifDay));
+        System.out.println("Your requested day is: " + ConverterIf.convertDay(ifDay) + "\n");
         // return day from switch
-        System.out.println("Please enter the MONTH Number for the Switch to convert:");
+        System.out.println("Please enter the Number you would like the Switch Converter to convert to a Month: ");
         int switchMonth = Integer.parseInt(scanner.nextLine());
-        System.out.println(ConverterSwitch.convertMonth(switchMonth));
+        System.out.println("Your requested month is: " + ConverterSwitch.convertMonth(switchMonth) + "\n");
         // return month from if
         // enter day number
-        System.out.println("Please enter the DAY Number for the Switch to convert:");
+        System.out.println("Please enter the Number for the Switch Converter to convert to a Day:");
         int switchDay = Integer.parseInt(scanner.nextLine());
-        System.out.println(ConverterSwitch.convertDay(switchDay));
+        System.out.println("Your requested day is: " + ConverterSwitch.convertDay(switchDay) + "\n");
         // return day from if
-
+        System.out.println("Thank You for your participation...rerun to play again");
     }
 
 }
