b1442fdaea9f97d1d8621e1777ca3cc6f95e9e3d
Dale Kittendorf <drkittendorf@gmail.com>
2021-06-09 22:52:28 -0500

    randomw word and random quote working

--- M2-Challenge-Kittendorf-Dale/src/main/java/com/challenge/M2ChallengeKittendorfDale/controller/QuoteController.java
+++ M2-Challenge-Kittendorf-Dale/src/main/java/com/challenge/M2ChallengeKittendorfDale/controller/QuoteController.java
@@ -18,22 +18,34 @@ public class QuoteController {
     private static List<Quote> quoteList= new ArrayList<> (Arrays.asList(
             new Quote (1,
                     "John Mason Goode",
-                    "Happiness consists in activity. It is running steam, not a stagnant pool."),
+                    "Happiness consists in activity. It is running stream, not a stagnant pool."),
             new Quote (2,
                     "Abraham Lincoln",
-                    "I will read and prepare myself and one day, my time will come"),
+                    "I'm a success today because I had a friend who believed in me and I didn't have the heart to let him down."),
             new Quote (3,
                     "Rene Descartes",
-                "It is not enough to have a good mind; the main thing is to use it well."),
+                    "It is not enough to have a good mind; the main thing is to use it well."),
             new Quote (4,
                     "Albert Einstein",
                     "It is not that I'm so smart.But I stay with the questions much longer."),
             new Quote (5,
                     "Johnathan Swift",
-                    "May You live every day of your life.")
-
-
-
+                    "May You live every day of your life."),
+            new Quote (6,
+                    "Theodore Roosevelt",
+                   "Far and away the best prize that life has to offer is the chance to work hard at work worth doing."),
+            new Quote (7,
+                   "Marion Zimmer Bradley",
+                   "It has never been, and never will be, easy work! But the road that is built in hope is more pleasant to the traveler than the road built in despair, even though they both lead to the same destination."),
+            new Quote (8,
+                   "Og Mandino",
+                   "The great difference between those who succeed and those who fail does not consist in the amount of work done by each but in the amount of intelligent work."),
+            new Quote (9,
+                   "Friedrich Nietzsche",
+                   "“All truly great thoughts are conceived while walking"),
+            new Quote (10,
+                   "Tony Robbins",
+                   "Changing an organization, a company, a country—or a world—begins with the simple step of changing yourself.")
             ));
 
     @RequestMapping(value = "/quote", method = RequestMethod.GET)
--- M2-Challenge-Kittendorf-Dale/src/main/java/com/challenge/M2ChallengeKittendorfDale/controller/WordController.java
+++ M2-Challenge-Kittendorf-Dale/src/main/java/com/challenge/M2ChallengeKittendorfDale/controller/WordController.java
@@ -1,7 +1,67 @@
 package com.challenge.M2ChallengeKittendorfDale.controller;
 
-public class WordController {
+import com.challenge.M2ChallengeKittendorfDale.models.Quote;
+import com.challenge.M2ChallengeKittendorfDale.models.Word;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
+
+@RestController
+public class WordController {
+    Random random = new Random();
+
+    private static List<Word> wordList= new ArrayList<>(Arrays.asList(
+            new Word(1,
+                    "Lexiphanes",
+                    "One who uses words pretentiously."),
+            new Word (2,
+                    "Wiseacre",
+                    "One who pretends to knowledge or cleverness; a would-be wise person; a smart aleck."),
+            new Word (3,
+                    "Transcendental",
+                    "Relating to a spiritual or nonphysical realm."),
+            new Word (4,
+                    "Matrix",
+                    "Something within or from which something else originates, develops, or takes form."),
+            new Word (5,
+                    "Vector",
+                    "A one-dimensional array data structure."),
+            new Word(6,
+                    "Proton",
+                    "A stable subatomic particle occurring in all atomic nuclei, with a positive electric charge equal in magnitude to that of an electron, but of opposite sign."),
+            new Word (7,
+                    "Dystopia",
+                    "An imagined state or society in which there is great suffering or injustice, typically one that is totalitarian or post-apocalyptic."),
+            new Word (8,
+                    "Ensorcell",
+                    "To cast a spell, enchant; to captivate."),
+            new Word (9,
+                    "Susurrus",
+                    "A whispering or rustling sound."),
+            new Word (10,
+                    "Nova",
+                    "A transient astronomical event that causes the sudden appearance of a bright, apparently new star, that slowly fades over several weeks or many months.")
+
+
+
+    ));
+
+    @GetMapping(value = "/word")
+    @ResponseStatus(HttpStatus.OK)
+    public Word getWord() {
+        Word newWord = new Word();
+
+        int randomWordIndex = random.nextInt(wordList.size());
+       Word randomWord = wordList.get(randomWordIndex);
+        System.out.println(randomWord);
+
+
+        return randomWord;
+    };
 
 }
 
