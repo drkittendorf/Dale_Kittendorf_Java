3cf1ae881e7c603ad7e15a7f124c9d5a78f3323e
Dale Kittendorf <drkittendorf@gmail.com>
2021-06-09 19:07:58 -0500

    extracted spring initializer

---
+++ M2-Challenge-Kittendorf-Dale/src/main/java/com/challenge/M2ChallengeKittendorfDale/M2ChallengeKittendorfDaleApplication.java
@@ -0,0 +1,13 @@
+package com.challenge.M2ChallengeKittendorfDale;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class M2ChallengeKittendorfDaleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(M2ChallengeKittendorfDaleApplication.class, args);
+	}
+
+}
---
+++ M2-Challenge-Kittendorf-Dale/src/test/java/com/challenge/M2ChallengeKittendorfDale/M2ChallengeKittendorfDaleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.challenge.M2ChallengeKittendorfDale;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class M2ChallengeKittendorfDaleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
